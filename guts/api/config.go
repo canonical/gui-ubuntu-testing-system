719d2202d4559a840470f554ad90b5d14a3d3347
Tim Andersson <tim.andersson@canonical.com>
2025-08-12 12:58:33 +0100

    new: add helper go functionality for api bootstrapping
    This commit adds a few basic go files with functionality for
    bootstrapping the API.

---
+++ guts/api/config.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "gopkg.in/yaml.v3"
+  "os"
+  "path/filepath"
+)
+
+
+type GutsApiConfig struct {
+  Postgres struct {
+    Host string `yaml:"hostname"`
+    Port int `yaml:"port"`
+    User string `yaml:"username"`
+    Password string `yaml:"password"`
+    DbName string `yaml:"dbname"`
+  }
+  Api struct {
+    Hostname string `yaml:"hostname"`
+    Port int `yaml:"port"`
+  }
+}
+
+func ParseConfig(filePath string) (GutsApiConfig, error) {
+  var config GutsApiConfig
+  filename, _ := filepath.Abs(filePath)
+  // yamlFile, err := ioutil.ReadFile(filename)
+  yamlFile, err := os.ReadFile(filename)
+  if err != nil {
+    return config, err
+  }
+  err = yaml.Unmarshal(yamlFile, &config)
+  return config, err
+}
