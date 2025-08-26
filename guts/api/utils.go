765849bd3a81043ed084440b7825f89dff1e1e78
Tim Andersson <tim.andersson@canonical.com>
2025-08-26 11:17:05 +0100

    utils.go: use regex to check if string is uuid

--- guts/api/utils.go
+++ guts/api/utils.go
@@ -6,27 +6,7 @@ import (
   "regexp"
 )
 
-func CheckStringIsAlphanumeric(testString string) bool {
-  return regexp.MustCompile(`^[a-zA-Z0-9]*$`).MatchString(testString)
-}
-
 func ValidateUuid(Uuid string) error {
-  parts := strings.Split(Uuid, `-`)
-  lengths := []int{8, 4, 4, 4, 12}
-
-  if !reflect.DeepEqual(len(lengths), len(parts)) {
-    return InvalidUuidError{uuid: Uuid}
-  }
-
-  for idx, length := range(lengths) {
-    thisSegment := parts[idx]
-    if !reflect.DeepEqual(len(thisSegment), length) {
-      return InvalidUuidError{uuid: Uuid}
-    }
-    if !CheckStringIsAlphanumeric(thisSegment) {
-      return InvalidUuidError{uuid: Uuid}
-    }
-  }
-  return nil
+  return regexp.MustCompile(`[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`).MatchString(Uuid)
 }
 
