efe113722d28f18caa3d507e3a3538515e25e9e0
Tim Andersson <tim.andersson@canonical.com>
2025-08-26 11:16:44 +0100

    flags.go: command line flag -> env var -> default

--- guts/api/flags.go
+++ guts/api/flags.go
@@ -3,6 +3,7 @@ package main
 import (
   "flag"
   "database/sql"
+  "os"
 )
 
 var (
@@ -11,7 +12,11 @@ var (
 )
 
 func ParseArgs() {
-  flag.StringVar(&configFilePath, "cfg-path", "./guts-api.yaml", "Path to config file for the guts api")
+  cfgPathFromEnv := os.GetEnv("GUTS_CFG_PATH")
+  if cfgPathFromEnv == "" {
+    cfgPathFromEnv = "/etc/guts-api.yaml"
+  }
+  flag.StringVar(&configFilePath, "cfg-path", cfgPathFromEnv, "Path to config file for the guts api")
   flag.Parse()
 }
 
