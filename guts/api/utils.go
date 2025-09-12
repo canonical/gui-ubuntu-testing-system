0f7e16c5014503a5759cc1e371bc7d2a9aa1c75c
Tim Andersson <tim.andersson@canonical.com>
2025-09-12 15:56:44 +0100

    Add utils.go: helper functions for API (incl utils_test.go)

---
+++ guts/api/utils.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func ValidateUuid(Uuid string) error {
+	if regexp.MustCompile(`[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`).MatchString(Uuid) {
+		return nil
+	}
+	return InvalidUuidError{uuid: Uuid}
+}
+
+func FileOrDirExists(path string) error {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("%v doesn't exist", path)
+		} else { // coverage-ignore
+			return err
+		}
+	}
+	return nil
+}
+
+func AllFilesExist(paths ...string) bool {
+	for i := 0; i < len(paths); i++ {
+		err := FileOrDirExists(paths[i])
+		if err != nil {
+			return false
+		}
+	}
+	return true
+}
+
+func AtomicWrite(data []byte, filename string) error {
+	newFile := fmt.Sprintf("%v.new", filename)
+	err := os.WriteFile(newFile, data, 0644)
+	if err != nil { // coverage-ignore
+		return err
+	}
+	err = os.Rename(newFile, filename)
+	if err != nil { // coverage-ignore
+		return err
+	}
+	return nil
+}
+
+func CreateDirIfNotExists(directory string) error {
+	// Creates a directory if it doesn't exist
+	_, err := os.Open(directory)
+	if err != nil {
+		err = os.Mkdir(directory, 0755)
+		return err
+	}
+	return nil
+}
+
+func GetDirSize(directory string) (int, error) {
+	// Gets the total size in bytes of a directory
+	DirSize = 0
+	err := filepath.Walk(directory,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil { // coverage-ignore
+				return err
+			}
+			DirSize += int(info.Size())
+			return nil
+		})
+	if err != nil { // coverage-ignore
+		return DirSize, err
+	}
+	return DirSize, err
+}
+
+func IsValidUrl(urlToTest string) bool {
+	u, err := url.Parse(urlToTest)
+	if err != nil {
+		return false
+	}
+	return u.Scheme != "" && u.Host != ""
+}
+
+func DownloadFile(url string) ([]byte, error) {
+	var b []byte
+	resp, err := http.Get(url)
+	if err != nil {
+		return b, err
+	}
+	defer DeferredErrCheck(resp.Body.Close)
+	b, err = io.ReadAll(resp.Body)
+	if err != nil { // coverage-ignore
+		return b, err
+	}
+	if len(b) == 0 {
+		return b, fmt.Errorf("file at %v is empty", url)
+	}
+	return b, nil
+}
---
+++ guts/api/utils_test.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestValidateUuid(t *testing.T) {
+	Uuid := "27549483-e8f5-497f-a05d-e6d8e67a8e8a"
+	err := ValidateUuid(Uuid)
+	reflect.DeepEqual(Uuid, "")
+	CheckError(err)
+}
+
+func TestValidateUuidFails(t *testing.T) {
+	Uuid := "farnsworth"
+	err := ValidateUuid(Uuid)
+	var expectedType InvalidUuidError
+	if !reflect.DeepEqual(reflect.TypeOf(err), reflect.TypeOf(expectedType)) {
+		t.Errorf("Unexpected error type!")
+	}
+}
+
+func TestValidateUuidIncorrectSegmentLengthFailure(t *testing.T) {
+	Uuid := "farnsworth-farnsworth-farnsworth-farnsworth-farnsworth"
+	err := ValidateUuid(Uuid)
+	var expectedType InvalidUuidError
+	if !reflect.DeepEqual(reflect.TypeOf(err), reflect.TypeOf(expectedType)) {
+		t.Errorf("Unexpected error type!")
+	}
+}
+
+func TestValidateUuidNonAlphanumericFailure(t *testing.T) {
+	Uuid := "!!!!!!!!-!!!!-!!!!-!!!!-!!!!!!!!!!!!"
+	err := ValidateUuid(Uuid)
+	var expectedType InvalidUuidError
+	if !reflect.DeepEqual(reflect.TypeOf(err), reflect.TypeOf(expectedType)) {
+		t.Errorf("Unexpected error type!")
+	}
+}
+
+func TestCreateDirIfNotExists(t *testing.T) {
+	dummyDir := "/tmp/my-dummy-directory/"
+	err := CreateDirIfNotExists(dummyDir)
+	CheckError(err)
+	_, err = os.Open(dummyDir)
+	CheckError(err)
+}
+
+func TestCreateDirIfNotExistsDirExistsAlready(t *testing.T) {
+	dummyDir := "/tmp/my-dummy-directory/"
+	_, err := os.Open(dummyDir)
+	if err == nil {
+		err = os.RemoveAll(dummyDir)
+		CheckError(err)
+	}
+
+	err = CreateDirIfNotExists(dummyDir)
+	CheckError(err)
+	_, err = os.Open(dummyDir)
+	CheckError(err)
+
+	err = CreateDirIfNotExists(dummyDir)
+	CheckError(err)
+	_, err = os.Open(dummyDir)
+	CheckError(err)
+}
+
+func TestIsValidUrl(t *testing.T) {
+	validUrl := "http://localhost:9999/res-1.tar.gz"
+	valid := IsValidUrl(validUrl)
+	if !valid {
+		t.Errorf("%v misidentified as an invalid url", validUrl)
+	}
+}
+
+func TestIsValidUrlInvalid(t *testing.T) {
+	invalidUrl := "\b Farnsworth is not a url."
+	valid := IsValidUrl(invalidUrl)
+	if valid {
+		t.Errorf("%v misidentified as a valid url", invalidUrl)
+	}
+}
+
+func TestDownloadFile(t *testing.T) {
+	ServeDirectory()
+	artifactUrl := "http://localhost:9999/res-1.tar.gz"
+	_, err := DownloadFile(artifactUrl)
+	CheckError(err)
+}
+
+func TestDownloadFileEmpty(t *testing.T) {
+	artifactUrl := "http://localhost:9999/empty.tar.gz"
+	_, err := DownloadFile(artifactUrl)
+	expectedErrString := "file at http://localhost:9999/empty.tar.gz is empty"
+	if !reflect.DeepEqual(err.Error(), expectedErrString) {
+		t.Errorf("Unexpected err string!\nExpected: %v\nActual: %v", expectedErrString, err.Error())
+	}
+}
