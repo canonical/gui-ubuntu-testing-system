9ee663af2cbe44ec8a56a12e65b1a29923fa58de
Tim Andersson <tim.andersson@canonical.com>
2025-09-12 16:10:59 +0100

    Implement functionality for job endpoint

---
+++ guts/api/job.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"github.com/lib/pq"
+	"time"
+)
+
+var (
+	AllJobColumns = []string{"uuid", "artifact_url", "tests_repo", "tests_repo_branch", "tests_plans", "image_url", "reporter", "status", "submitted_at", "requester", "debug", "priority"}
+)
+
+type JobEntry struct {
+	Uuid            string    `json:"uuid"`
+	ArtifactUrl     *string   `json:"artifact_url"`
+	TestsRepo       string    `json:"tests_repo"`
+	TestsRepoBranch string    `json:"tests_repo_branch"`
+	TestsPlans      []string  `json:"tests_plans"`
+	ImageUrl        string    `json:"image_url"`
+	Reporter        string    `json:"reporter"`
+	Status          string    `json:"status"`
+	SubmittedAt     time.Time `json:"submitted_at"`
+	Requester       string    `json:"requester"`
+	Debug           bool      `json:"debug"`
+	Priority        int       `json:"priority"`
+}
+
+type JobWithTestsDetails struct {
+	Job     JobEntry
+	Results map[string]string `json:"results"`
+}
+
+type ReturnableJson interface {
+	toJson()
+}
+
+func (j JobEntry) toJson() string {
+	b, err := json.Marshal(j)
+	if err != nil { // coverage-ignore
+		return ""
+	}
+	return string(b)
+}
+
+func (j JobWithTestsDetails) toJson() string {
+	b, err := json.Marshal(j)
+	if err != nil { // coverage-ignore
+		return ""
+	}
+	return string(b)
+}
+
+func GetCompleteResultsForUuid(uuidToFind string) (JobWithTestsDetails, error) {
+	var completeJob JobWithTestsDetails
+	job, err := FindJobByUuid(uuidToFind)
+	if err != nil {
+		return completeJob, err
+	}
+
+	testResults, err := CollateUuidTestResults(uuidToFind)
+	if err != nil { // coverage-ignore
+		return completeJob, err
+	}
+
+	completeJob.Job = job
+	completeJob.Results = testResults
+
+	return completeJob, nil
+}
+
+func CollateUuidTestResults(uuidToFind string) (map[string]string, error) {
+	testResults := make(map[string]string)
+
+	var params = []string{"test_case", "state"}
+	rows, err := Driver.Query("tests", "uuid", uuidToFind, params)
+	if err != nil { // coverage-ignore
+		return testResults, err
+	}
+	defer DeferredErrCheck(rows.Close)
+
+	for rows.Next() {
+		var testCase string
+		var state string
+		err := rows.Scan(&testCase, &state)
+		if err != nil { // coverage-ignore
+			return testResults, err
+		}
+		testResults[testCase] = state
+	}
+	return testResults, nil
+}
+
+func FindJobByUuid(uuidToFind string) (JobEntry, error) {
+	var job JobEntry
+
+	row, err := Driver.QueryRow("jobs", "uuid", uuidToFind, AllJobColumns)
+	if err != nil { // coverage-ignore
+		return job, err
+	}
+	err = row.Scan(
+		&job.Uuid,
+		&job.ArtifactUrl,
+		&job.TestsRepo,
+		&job.TestsRepoBranch,
+		pq.Array(&job.TestsPlans),
+		&job.ImageUrl,
+		&job.Reporter,
+		&job.Status,
+		&job.SubmittedAt,
+		&job.Requester,
+		&job.Debug,
+		&job.Priority,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return job, UuidNotFoundError{uuid: uuidToFind}
+		}
+		return job, err // coverage-ignore
+	}
+	return job, nil
+}
---
+++ guts/api/job_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestFindJobByUuid(t *testing.T) {
+	Uuid := "4ce9189f-561a-4886-aeef-1836f28b073b"
+	ParseArgs()
+	err := ParseConfig(configFilePath)
+	CheckError(err)
+	err = Setup()
+	if SkipTestIfPostgresInactive(err) {
+		t.Skip("Skipping test as postgresql service is not up")
+	} else {
+		CheckError(err)
+	}
+	job, err := FindJobByUuid(Uuid)
+	CheckError(err)
+	var TestJob JobEntry
+	TestJob.Uuid = "4ce9189f-561a-4886-aeef-1836f28b073b"
+	TestJob.ArtifactUrl = nil
+	TestJob.TestsRepo = "https://github.com/canonical/ubuntu-gui-testing.git"
+	TestJob.TestsRepoBranch = "main"
+	TestJob.TestsPlans = []string{"tests/firefox-example/plans/extended.yaml", "tests/firefox-example/plans/regular.yaml"}
+	TestJob.ImageUrl = "https://cdimage.ubuntu.com/daily-live/current/questing-desktop-amd64.iso"
+	TestJob.Reporter = "test_observer"
+	TestJob.Status = "running"
+	layout := "2006-01-02T15:04:05.999999"
+	value := "2025-07-23T14:17:14.632177"
+	TestJob.SubmittedAt, err = time.Parse(layout, value)
+	CheckError(err)
+	TestJob.Requester = "andersson123"
+	TestJob.Debug = false
+	TestJob.Priority = 8
+	if !reflect.DeepEqual(job, TestJob) {
+		t.Errorf("Expected job not the same as actual:\n%v\n%v", TestJob, job)
+	}
+}
+
+func TestJobToJson(t *testing.T) {
+	var TestJob JobEntry
+	TestJob.Uuid = "4ce9189f-561a-4886-aeef-1836f28b073b"
+	TestJob.ArtifactUrl = nil
+	TestJob.TestsRepo = "https://github.com/canonical/ubuntu-gui-testing.git"
+	TestJob.TestsRepoBranch = "main"
+	TestJob.TestsPlans = []string{"tests/firefox-example/plans/extended.yaml", "tests/firefox-example/plans/regular.yaml"}
+	TestJob.ImageUrl = "https://cdimage.ubuntu.com/daily-live/current/questing-desktop-amd64.iso"
+	TestJob.Reporter = "test_observer"
+	TestJob.Status = "running"
+	layout := "2006-01-02T15:04:05.999999Z"
+	value := "2025-07-23T14:17:14.632177Z"
+	parsedTime, err := time.Parse(layout, value)
+	CheckError(err)
+	TestJob.SubmittedAt = parsedTime
+	TestJob.Requester = "andersson123"
+	TestJob.Debug = false
+	TestJob.Priority = 8
+	ExpectedJson := `{"uuid":"4ce9189f-561a-4886-aeef-1836f28b073b","artifact_url":null,"tests_repo":"https://github.com/canonical/ubuntu-gui-testing.git","tests_repo_branch":"main","tests_plans":["tests/firefox-example/plans/extended.yaml","tests/firefox-example/plans/regular.yaml"],"image_url":"https://cdimage.ubuntu.com/daily-live/current/questing-desktop-amd64.iso","reporter":"test_observer","status":"running","submitted_at":"2025-07-23T14:17:14.632177Z","requester":"andersson123","debug":false,"priority":8}`
+	ConvertedJson := TestJob.toJson()
+	if !reflect.DeepEqual(ExpectedJson, ConvertedJson) {
+		t.Errorf("json conversion not as expected!\nExpected: %v\nActual: %v", ExpectedJson, ConvertedJson)
+	}
+}
